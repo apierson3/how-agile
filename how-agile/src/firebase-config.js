8da56f0ac1bdbb214c83739f05dd55ce9f0a1d4a
apierson3 <apierson3@gmail.com>
2024-11-07 21:28:58 -0600

    initial setup

---
+++ how-agile/src/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8hAWk4xWaSAoOVF2p6Wshfna5KW0JxTU",
+  authDomain: "how-agile.firebaseapp.com",
+  projectId: "how-agile",
+  storageBucket: "how-agile.firebasestorage.app",
+  messagingSenderId: "9814075305",
+  appId: "1:9814075305:web:ad431fa424faac760906b8",
+  measurementId: "G-8F2HV2Z4JQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
